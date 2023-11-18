00072087b3fba6cb5f1eab68f0cefcc42144958c
TQHuy <tqhuy16@gmail.com>
2023-11-18 12:38:19 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style scoped>
+/* .wrap-app {
+  min-height: 100vh;
+  background-color: #49bedb;
+} */
+/* @media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+} */
+@media only screen and (max-width: 576px) {
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+  <div class="header">
+    <div class="menu">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div>Spin & Win</div>
+    <font-awesome-icon icon="volume-high" />
+  </div>
+</template>
+
+<style scoped>
+@media only screen and (max-width: 576px) {
+  .header {
+    color: white;
+    display: flex;
+    justify-content: space-between;
+  }
+  .menu {
+    width: 20px;
+    height: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+  }
+  .menu div {
+    height: 2px;
+    width: 100%;
+    background-color: #ffffff;
+  }
+}
+</style>
